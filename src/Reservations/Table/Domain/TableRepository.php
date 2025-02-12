dca96b9df72c5d346433ec6f95d1e4969ad6079b
Hugo Langa <hugo.langa@shopery.com>
2025-02-13 00:00:07 +0100

    Create and implement the table repository

---
+++ src/Reservations/Table/Domain/TableRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LastReservation\Reservations\Table\Domain;
+
+interface TableRepository
+{
+    public function save(Table $table): void;
+    
+    public function findById(TableId $id): ?Table;
+    
+    public function findAll(): array;
+} 
